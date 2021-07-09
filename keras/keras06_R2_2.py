e8bb2a7b5d4dbc83270ba71f1bc2e7b19ebcb748
fairy93 <kimdongju3331@gmail.com>
2021-07-09 18:02:14 +0900

    r2 공부

--- keras/keras06_R2_2.py
+++ keras/keras06_R2_2.py
@@ -1,7 +1,9 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from keras import optimizers
 from sklearn.metrics import r2_score
 import numpy as np
+from tensorflow.python.ops.gen_dataset_ops import optimize_dataset
 
 # 출력 로스값
 x = np.array([1,2,3,4,5])
@@ -10,9 +12,14 @@ x_pred=[6]
 
 model = Sequential()
 model.add(Dense(1,input_dim=1))
+model.add(Dense(5))
+model.add(Dense(12))
+model.add(Dense(1))
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer='sgd')
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(x,y,epochs=5000,batch_size=1)
+model.fit(x,y,epochs=15000,batch_size=1)
 
 #4. 평가, 예측
 loss = model.evaluate(x,y) # 평가
@@ -23,3 +30,5 @@ print('x_pred의 예측값 : ',y_predict)
 
 r2= r2_score(y,y_predict)
 print(r2)
+
+# 과적합 = 오버피팅
---
+++ keras/keras07_boston.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_boston
+
+datasets = load_boston()
+x=datasets.data
+y=datasets.target
+
+print(x.shape)
+print(y.shape)
+
+print(datasets.feature_names)
+print(datasets.DESCR)
