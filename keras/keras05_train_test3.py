be1a67dc128328d2b0fca43a2a69c66ca109a01e
fairy93 <kimdongju3331@gmail.com>
2021-07-14 15:59:02 +0900

    keras mlp train, test study

--- keras/keras05_train_test3.py
+++ keras/keras05_train_test3.py
@@ -3,11 +3,8 @@ from tensorflow.keras.layers import Dense
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
-# 모델엔 2가지 있어 순차적모델, 함수형 모델
 
 #1. 데이터
-# 과적합? 내신잘봐봣자 수능못보면 소요없어
-# 훈련용 테스트 데이터 구분
 x=np.array(range(100))
 y=np.array(range(1,101))
 
@@ -34,10 +31,5 @@ model.fit(x_train,y_train,epochs=1000,batch_size=1)
 loss = model.evaluate(x_test,y_test) # 평가
 print('loss : ',loss)
 
-# result = model.predict(x_pred)
-# print('x_pred의 예측값 : ',result)
-
 y_predict = model.predict([100])
 print('y_predic 의 값은 ',y_predict)
-
-# 결정계수 회귀모델r2
