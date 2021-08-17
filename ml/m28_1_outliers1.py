307bd166f7b1a2c666d5159fe168391fb4c24bb2
fairy93 <kimdongju3331@gmail.com>
2021-08-17 21:36:12 +0900

    이상치 결측치 제거

---
+++ ml/m28_1_outliers1.py
@@ -0,0 +1,29 @@
+# 이상치처리
+#1. 삭제
+#2. nan처리후 -> 보간 // linear
+# ....................(결측치 처리 방법과 유사)
+
+import numpy as np
+aaa =np.array([1,2,-10000,4,5,6,7,8,90,100,5000])
+
+# 사분위의수 중위수 
+
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out,[25,50,75])
+    print("1사분위 : ",quartile_1)
+    print("q2 : ", q2)
+    print("3사분위 : ", quartile_3)
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 -(iqr*1.5)
+    upper_bound = quartile_3 -(iqr*1.5)
+    return np.where((data_out>upper_bound) | (data_out<lower_bound))
+
+outliers_loc = outliers(aaa)
+print('이상치의 위치  ',outliers_loc)
+
+# 시각화
+# 실습
+# 위데이터를 box플롯으로 그리시오
+import matplotlib.pyplot as plt
+plt.boxplot(aaa)
+plt.show()
